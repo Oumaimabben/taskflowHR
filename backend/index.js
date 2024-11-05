ba3148d404a30e8f694a3e2b9086f9dfe14f3ed6
manelaskry <manlaskry8@gmail.com>
2024-11-05 16:23:18 +0100

    init prisma - regenerate the Prisma client by adding binaryTargets to run prisma generate inside the backend container

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,42 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
 
 
 
 const app =express();
+const prisma = new PrismaClient();
 
+app.use(express.json());
+
+
+//cors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
+
+  //test api
+app.get('/test', (req, res) => {
+    try {
+      res.status(200).json({ message: 'API is working' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  //get all users
+app.get('/users', async (req, res) => {
+    try {
+      const users = await prisma.user.findMany();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+
+  
 app.listen(5002,()=>{
-    console.log("server running at port 5000")
+    console.log("server running at port 5002")
 });
